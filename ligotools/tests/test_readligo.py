36b33fc4be812be3d8fe63f57944c12af6fb5ca1
Runting Han <hrt@berkeley.edu>
2022-04-27 20:23:45 +0000

    utils test

--- ligotools/tests/test_readligo.py
+++ ligotools/tests/test_readligo.py
@@ -7,6 +7,7 @@ import json
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 from ligotools import readligo as rl
+from ligotools.utils import *
 
 eventname = 'GW150914' 
 fnjson = "data/BBH_events_v3.json"
@@ -33,3 +34,37 @@ def test_dq_channel_to_seglist1():
     
 def test_read_hdf5():
     assert len(rl.read_hdf5("data/H-H1_LOSC_4_V2-1126259446-32.hdf5", True)) == 7
+    
+    
+# tests for utils
+fs = event['fs'] 
+NFFT = 4*fs
+fband = event['fband'] 
+bb, ab = butter(4, [fband[0]*2./fs, fband[1]*2./fs], btype='band')
+normalization = np.sqrt((fband[1]-fband[0])/(fs/2))
+Pxx_H1, freqsH = mlab.psd(strain_H1, Fs = fs, NFFT = NFFT)
+Pxx_L1, freqsL = mlab.psd(strain_L1, Fs = fs, NFFT = NFFT)
+psd_H1 = interp1d(freqsH, Pxx_H1)
+psd_L1 = interp1d(freqsL, Pxx_L1)
+dt_H1 = time_H1[1] - time_H1[0]
+dt_L1 = time_L1[1] - time_L1[0]
+
+def test_whiten1():
+	whiten_h1 = whiten(strain_H1, psd_H1, dt_H1)
+	assert type(whiten_h1) == type(np.array([1,2]))
+    assert len(whiten_h1) == 131072
+    
+def test_whiten2():
+	whiten_l1 = whiten(strain_L1, psd_L1, dt_L1)
+	assert type(whiten_l1) == type(np.array([1,2]))
+
+def test_write_wavfile():
+	whiten_h1 = whiten(strain_H1, psd_H1, dt_H1)
+	write_wavfile("audio/" + eventname +"_H1_shifted.wav", 4096, whiten_h1)
+	assert path.isfile('audio/GW150914_H1_shifted.wav') == True
+
+def test_plot_code():
+	whiten_l1 = whiten(strain_L1, psd_L1, dt_L1)
+	plot_PSD(0, 1126259462.4324, 13.2, 'g', 'L1', 1126259462.44, 0, 0, 'GW150914', 'png', 0, 0, 999.74, 0, 0, 4096)
+	assert exists("figures/GW150914_L1_matchfreq.png")
+ 
